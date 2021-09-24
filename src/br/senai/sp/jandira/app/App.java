196048a905a726625f1321da199c355490cbd838
DANILO DA CRUZ MUNHE <21276321@CFP127ALUNO.LOCAL>
2021-09-24 16:43:43 -0300

    Primeira versao

---
+++ src/br/senai/sp/jandira/app/App.java
@@ -0,0 +1,31 @@
+package br.senai.sp.jandira.app;
+
+import br.senai.sp.jandira.model.Conta;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		//Criação da conta da Maria
+		Conta contaMaria = new Conta(); 
+		contaMaria.titular = "Maria Antonieta";
+		contaMaria.numero = "7845-8";
+		contaMaria.numeroAgencia = "4214-9";
+		contaMaria.saldo = 500.0;
+		contaMaria.tipo = "Corrente";
+		
+		//Criação da conta do Pedro
+		Conta contaPedro = new Conta();
+		contaPedro.titular = "Pedro Cabral";
+		contaPedro.numero = "6532-9";
+		contaPedro.numeroAgencia = "4214-9";
+		contaPedro.saldo = 200.0;
+		contaPedro.tipo = "Poupança";
+		
+		//Exibir os detalhes da conta da Maria
+		contaMaria.exibirDetalhes();
+		contaPedro.exibirDetalhes();
+		
+	}
+
+}
